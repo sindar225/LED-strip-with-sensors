2d9426b13b5e6b5a5c425063175cc25bc6e32cf2
Denis Tregubov <denis_tregubov@epam.com>
2019-11-12 21:14:39 +0300

    minor fix; BT commented

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,14 @@
 #define DATA_PIN    16
 #define NUM_LEDS    30
 #define BRIGHTNESS  255
+#define GRADIENT_PALLETE_END 200
 #define LED_TYPE    WS2812B
 
 CRGB leds[NUM_LEDS];
 
 DEFINE_GRADIENT_PALETTE( royal_BTe_gd ) {
   0,     0,  0,  0,   //black
-255,   65,  105,  225}; // CRGB::RoyalBTe
+255,   0,  0,  80}; // CRGB::RoyalBTe
 
 CRGBPalette16 myPal = royal_BTe_gd;
 SoftwareSerial BT(8, 7);
@@ -26,8 +27,8 @@ void switch_leds();
 uint8_t timeDelay = 5;  // time in ms.  Lower number is faster travel.
 
 void setup() {
-  Serial.begin(9600);
-  BT.begin(9600);
+  //Serial.begin(9600);
+  //BT.begin(9600);
   pinMode(TRIGGER, INPUT);
 
   // LED set-up
@@ -43,45 +44,42 @@ void loop() {
   if (!mic && claps <= 2) { // !mic is a clap // no more than 2 claps in a row
     claps += 1;
     if ((current_millis - first_clap_millis) >= 200 && (current_millis - first_clap_millis) <= 800) {
-      Serial.println("double mic!");
+      //Serial.println("double mic!");
       
       switch_leds();
 
       claps = 0;
     } // if
-
     first_clap_millis = current_millis;
-
     claps = 0;
   } // if 
 
-
-  if (leds[0].r > 0) {
-    fill_solid(leds, NUM_LEDS, ColorFromPalette(myPal, 200));
+  if (leds[0].r > 0 && leds[0].g > 0 && leds[0].b > 0 ) {
+    fill_solid(leds, NUM_LEDS, ColorFromPalette(myPal, GRADIENT_PALLETE_END));
     FastLED.show();
-    
-    while (BT.available() > 0) {
-      int redInt = BT.parseInt();
-      int greenInt = BT.parseInt();
-      int BTeInt = BT.parseInt();
-      redInt = constrain(redInt, 0, 255);
-      greenInt = constrain(greenInt, 0, 255);
-      BTeInt = constrain(BTeInt, 0, 255);
-      if (BT.available() > 0)
-      {
-        // Serial.print("Red: ");
-        // Serial.print(redInt);
-        // Serial.print(" Green: ");
-        // Serial.print(greenInt);
-        // Serial.print(" BTe: ");
-        // Serial.print(BTeInt);
-        // Serial.println();
-        fill_solid(leds, NUM_LEDS, CRGB(redInt, greenInt, BTeInt));
-        FastLED.show();
-        BT.flush();
-      }
-    }
   }
+  //   while (BT.available() > 0) {
+  //     int redInt = BT.parseInt();
+  //     int greenInt = BT.parseInt();
+  //     int BTeInt = BT.parseInt();
+  //     redInt = constrain(redInt, 0, 255);
+  //     greenInt = constrain(greenInt, 0, 255);
+  //     BTeInt = constrain(BTeInt, 0, 255);
+  //     if (BT.available() > 0)
+  //     {
+  //       // Serial.print("Red: ");
+  //       // Serial.print(redInt);
+  //       // Serial.print(" Green: ");
+  //       // Serial.print(greenInt);
+  //       // Serial.print(" BTe: ");
+  //       // Serial.print(BTeInt);
+  //       // Serial.println();
+  //       fill_solid(leds, NUM_LEDS, CRGB(redInt, greenInt, BTeInt));
+  //       FastLED.show();
+  //       BT.flush();
+  //     }
+  //   }
+  // }
 }
 /*
   Toggle LEDs on and off. You can only
@@ -89,13 +87,12 @@ void loop() {
 */
 void switch_leds() {
   //Serial.println(leds[0].r);
-  if (leds[0].r == 0) {
-    for (uint16_t i = 0; i < NUM_LEDS / 2; i++) {
-      for (int index = 0; index <= 200; index += 20) {
+  if (leds[0].r == 0 && leds[0].g == 0 && leds[0].b == 0 ) {
+    for (uint16_t i = 0; i <= NUM_LEDS / 2; i++) {
+      for (int index = 0; index <= GRADIENT_PALLETE_END; index += 20) {
         leds[i] = ColorFromPalette(myPal, index);
         leds[NUM_LEDS - i] = ColorFromPalette(myPal, index);
 
-        //if (i == NUM_LEDS - i) { return; };
         FastLED.show();
         delay(10);
       }// for
@@ -104,7 +101,7 @@ void switch_leds() {
   } else {
     //Serial.println("LOW");
     for (uint16_t i = 15; i <= NUM_LEDS; i++) {
-      for (int index = 200; index >= 0; index -= 20) {
+      for (int index = GRADIENT_PALLETE_END; index >= 0; index -= 20) {
         leds[i] = ColorFromPalette(myPal, index);
         leds[NUM_LEDS - i] = ColorFromPalette(myPal, index);
 
